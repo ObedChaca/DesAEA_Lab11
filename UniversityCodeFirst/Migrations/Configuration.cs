bc3724f2ccc0693cdb849183a875f8059d75cf25
Obed Chaca <obed.chaca@tecsup.edu.pe>
2021-10-29 22:06:44 -0500

    Agregar archivos de proyecto.

---
+++ UniversityCodeFirst/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace UniversityCodeFirst.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<UniversityCodeFirst.SchoolContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(UniversityCodeFirst.SchoolContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
